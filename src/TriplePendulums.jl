0893a3e351335f30b58b175ec2d5397718df53b2
Lilith Orion Hafner <lilithhafner@gmail.com>
2021-10-18 09:52:04 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.20

---
+++ src/TriplePendulums.jl
@@ -0,0 +1,5 @@
+module TriplePendulums
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TriplePendulums
+using Test
+
+@testset "TriplePendulums.jl" begin
+    # Write your tests here.
+end
